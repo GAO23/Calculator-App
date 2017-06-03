1b9e50ad248c1fadba2d4399f4c71b873ba0c354
GAO23 <gaoxiangshuai@gmail.com>
2017-06-03 01:36:05 -0400

    More progress
    Committed 6/3/17

--- app/src/main/java/io/github/gao23/myapplication/NewTipActivity.java
+++ app/src/main/java/io/github/gao23/myapplication/NewTipActivity.java
@@ -44,81 +44,81 @@ public class NewTipActivity extends AppCompatActivity {
         subEntry.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
         entry.addTextChangedListener(new TextChangeListener(saveButton, entry, subEntry, customerPayment, this));
         subEntry.addTextChangedListener(new TextChangeListener(this.saveButton, entry, subEntry, customerPayment, this));
-        lp1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        lp1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         lp1.addRule(RelativeLayout.BELOW, entry.getId());
-        lp2 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        lp2 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         lp2.addRule(RelativeLayout.BELOW, subEntry.getId());
-     }
+    }
 
-    public void newUnpaidOrderClicked(View v){
+    public void newUnpaidOrderClicked(View v) {
         entry.setText("");
         subEntry.setText("");
         entry.setHint("Enter amount charged to customer");
         subEntry.setHint("Enter your order number");
-        if(!viewAddedFlag){
+        if (!viewAddedFlag) {
             view.addView(entry);
             view.addView(subEntry, lp1);
             this.viewAddedFlag = true;
         }
-        if(cashTip!= null){
+        if (cashTip != null) {
             view.removeView(cashTip);
-            cashTip=null;
+            cashTip = null;
         }
-        if(customerPayment==null) {
+        if (customerPayment == null) {
             customerPayment = new EditText(this);
             customerPayment.setHint("Enter customer payment amount");
             customerPayment.addTextChangedListener(new TextChangeListener(this.saveButton, entry, subEntry, customerPayment, this));
             view.addView(customerPayment, lp2);
         }
-        if(customerPayment!=null){
+        if (customerPayment != null) {
             customerPayment.setText("");
             customerPayment.setHint("Enter customer payment amount");
         }
     }
 
-    public void newPaidOrderClicked(View v){
+    public void newPaidOrderClicked(View v) {
         entry.setText("");
         subEntry.setText("");
         entry.setHint("Enter your tip amount");
         subEntry.setHint("Enter your order address");
 
-        if(!viewAddedFlag){
+        if (!viewAddedFlag) {
             view.addView(entry);
             view.addView(subEntry, lp1);
             this.viewAddedFlag = true;
         }
-        if(customerPayment!=null){
+        if (customerPayment != null) {
             view.removeView(customerPayment);
             customerPayment = null;
         }
-        if(cashTip== null) {
+        if (cashTip == null) {
             cashTip = new CheckBox(this);
             cashTip.setText("Check the box if this is cash tip");
             view.addView(cashTip, lp2);
         }
-        if(cashTip!=null){
+        if (cashTip != null) {
             cashTip.setChecked(false);
         }
     }
 
-    public boolean isCashTipNull(){
+    public boolean isCashTipNull() {
         return cashTip == null;
     }
 
     public void SaveClicked(View v) {
         Entry result;
         try {
-            if(isCashTipNull()) {
+            if (isCashTipNull()) {
                 double chargedAmount;
                 int orderNum;
                 double payment;
                 try {
                     chargedAmount = Double.parseDouble(entry.getText().toString());
-                    if(!decimalCheck(chargedAmount)){
+                    if (!decimalCheck(chargedAmount)) {
                         throw new InvalidInputException(5);
                     }
                 } catch (NumberFormatException e) {
-                   throw new InvalidInputException(1);
+                    throw new InvalidInputException(1);
                 }
                 try {
                     orderNum = Integer.parseInt(subEntry.getText().toString());
@@ -127,57 +127,70 @@ public class NewTipActivity extends AppCompatActivity {
                 }
                 try {
                     payment = Double.parseDouble(customerPayment.getText().toString());
-                    if(!decimalCheck(payment)){
+                    if (!decimalCheck(payment)) {
                         throw new InvalidInputException(6);
                     }
                 } catch (NumberFormatException e) {
                     throw new InvalidInputException(3);
                 }
-                result = new Entry(chargedAmount,orderNum,payment);
-               this.terminate(result);
-            }
-            else{
+                result = new Entry(chargedAmount, orderNum, payment);
+                this.terminate(result);
+            } else {
                 double tipAmount;
                 String orderAddress = subEntry.getText().toString();
                 boolean isCashTip = cashTip.isChecked();
                 try {
                     tipAmount = Double.parseDouble(entry.getText().toString());
-                    if(!decimalCheck(tipAmount)){
+                    if (!decimalCheck(tipAmount)) {
                         throw new InvalidInputException(7);
                     }
-                }catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
                     throw new InvalidInputException(4);
                 }
-                result = new Entry(tipAmount,orderAddress,isCashTip);
+                result = new Entry(tipAmount, orderAddress, isCashTip);
                 this.terminate(result);
             }
-        }
-        catch (InvalidInputException e){
+        } catch (InvalidInputException e) {
             int errorCode = e.getErrorCode();
-            switch(errorCode) {
-                case 1: Toast.makeText(this.getApplicationContext(), "Invalid charged amount", Toast.LENGTH_LONG).show();
+            switch (errorCode) {
+                case 1:
+                    Toast.makeText(this.getApplicationContext(), "Invalid charged amount", Toast.LENGTH_LONG).show();
                     break;
-                case 2: Toast.makeText(this.getApplicationContext(), "Order number needs to be a whole number", Toast.LENGTH_LONG).show();
+                case 2:
+                    Toast.makeText(this.getApplicationContext(), "Order number needs to be a whole number", Toast.LENGTH_LONG).show();
                     break;
-                case 3: Toast.makeText(this.getApplicationContext(), "Invalid payment amount", Toast.LENGTH_LONG).show();
+                case 3:
+                    Toast.makeText(this.getApplicationContext(), "Invalid payment amount", Toast.LENGTH_LONG).show();
                     break;
-                case 4: Toast.makeText(this.getApplicationContext(), "Invalid tip amount", Toast.LENGTH_LONG).show();
+                case 4:
+                    Toast.makeText(this.getApplicationContext(), "Invalid tip amount", Toast.LENGTH_LONG).show();
                     break;
-                case 5 : Toast.makeText(this.getApplicationContext(), "Make sure tip amount has two decimals only for cents", Toast.LENGTH_LONG).show();
+                case 5:
+                    Toast.makeText(this.getApplicationContext(), "Make sure tip amount has two decimals only for cents", Toast.LENGTH_LONG).show();
                     break;
-                case 6 : Toast.makeText(this.getApplicationContext(), "Make sure payment amount has two decimals only for cents", Toast.LENGTH_LONG).show();
+                case 6:
+                    Toast.makeText(this.getApplicationContext(), "Make sure payment amount has two decimals only for cents", Toast.LENGTH_LONG).show();
                     break;
-                case 7 : Toast.makeText(this.getApplicationContext(), "Make sure tip amount has two decimals for only cents", Toast.LENGTH_LONG).show();
+                case 7:
+                    Toast.makeText(this.getApplicationContext(), "Make sure tip amount has two decimals for only cents", Toast.LENGTH_LONG).show();
             }
         }
     }
 
     private boolean decimalCheck(double test) {
-        String text = Double.toString(Math.abs(test));
-        int integerPlaces = text.indexOf('.');
-        int decimalPlaces = text.length() - integerPlaces - 1;
-       return decimalPlaces == 2;
-    }
+        String num =  Double.toString(test);  // I assume that is `String` because entered by user and not yet converted to `double`
+        int i = num.lastIndexOf('.');
+        if(test % 1 == 0){
+            return true;
+        }
+        else if(i != -1 && (num.substring(i + 1).length() == 2 || num.substring(i + 1).length() == 1)) {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+}
 
 
       private void terminate(Entry result){
